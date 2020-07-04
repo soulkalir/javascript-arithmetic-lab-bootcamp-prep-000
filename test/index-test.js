0708ebc76a1ee34fb7df762117e33457a0de512b
Andres Rivera <soulkalir@gmail.com>
2020-07-04 04:07:55 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -73,3 +73,20 @@ describe('preserveDecimal(n)', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
   })
 })
+
+
+function add(a, b){
+  return a + b
+}
+
+function substract(a, b){
+  return b - a
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide( a, b){
+  return a/b
+}
